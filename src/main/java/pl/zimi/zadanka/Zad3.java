a0d738bb9ef252c4d1f2a02e894a7e04a69b8064
sm000k <smok.lacki@gmail.com>
2021-03-11 22:13:29 +0100

    Zad3 dopieszczamy kod pierwsza iteracja

--- src/main/java/pl/zimi/zadanka/Zad3.java
+++ src/main/java/pl/zimi/zadanka/Zad3.java
@@ -2,24 +2,21 @@ package pl.zimi.zadanka;
 
 public class Zad3 {
     public boolean checkPowerOfTen(Integer inputNumber) {
-        boolean answer = false;
-        if (inputNumber.equals(1) || inputNumber.equals(10))
-            answer = true;
-        else {
-            answer = false;
+
+        if (inputNumber.equals(1)) {
+            return true;
         }
+
         Integer tempNumber = inputNumber;
 
         while (tempNumber % 10 == 0) {
             tempNumber = tempNumber / 10;
 
-            if (tempNumber == 10) {
+            if (tempNumber == 1) {
                 return true;
             }
         }
-
-
-        return answer;
+        return false;
     }
 
     ;
