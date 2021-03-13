2240a4f7d13ef80de634a6654c403b5fa14e222e
sm000k <smok.lacki@gmail.com>
2021-03-13 12:31:27 +0100

    Zad4 commit poprawnego 1-testu prymitywny przykład

--- src/main/java/pl/zimi/zadanka/Zad4.java
+++ src/main/java/pl/zimi/zadanka/Zad4.java
@@ -1,8 +1,29 @@
 package pl.zimi.zadanka;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 source
 https://leetcode.com/problems/reverse-words-in-a-string/
  */
-public class Zad4
-{
+public class Zad4 {
+    public static String reverseString(String s) {
+        char c = s.charAt(0);
+        String sReverse = new String();
+        for (int head = 0; head < s.length(); head++) {
+            sReverse += String.valueOf(s.charAt(s.length() - head - 1));
+            System.out.println(sReverse);
+        }
+        System.out.println(sReverse);
+        return sReverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main");
+//        System.out.println(new Zad2().missingNumber(inputNumbers.toArray(new Integer[inputNumbers.size()])));
+        Zad4 klasa = new Zad4();
+        String reversedinput= klasa.reverseString("string");
+
+    }
 }
---
+++ src/test/java/pl/zimi/zadanka/Zad4Test.java
@@ -0,0 +1,15 @@
+package pl.zimi.zadanka;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Zad4Test {
+    @Test
+    void checkstring() {
+
+        Zad4 object = new Zad4();
+
+        Assertions.assertEquals("gnirts", object.reverseString("string"));
+
+    }
+}
