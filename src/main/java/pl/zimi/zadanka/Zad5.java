29301ff1f438e51a27900dac83a8176aa25cb923
sm000k <smok.lacki@gmail.com>
2021-03-22 01:01:57 +0100

    Zad 5 Dodanie moich wypocin przy ciągu fibbonaciego. Wrzucam jako dowód włożonej pracy. Nie polecam sprawdzać.

--- src/main/java/pl/zimi/zadanka/Zad5.java
+++ src/main/java/pl/zimi/zadanka/Zad5.java
@@ -1,5 +1,6 @@
 package pl.zimi.zadanka;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,17 +9,64 @@ source:
 https://leetcode.com/problems/climbing-stairs/
  */
 public class Zad5 {
+//    public void Node ( int destination,int currentPath){
+//        int left = currentPath+1;
+//        int right = destination+2;
+//    }
+    /*
+    key = destination pointer=current node
+    left/right child node
+     */
+    public static int climbStairs(int key) {
+        int solutionCounter = 0;
+        int startPointer=1;
+        int pointer =startPointer;
+        int left=1;
+        int right=2;
 
-    public static int getRandomNumber(int min, int max) {
+        while ((pointer!=0)&&(pointer!=key)){
 
-        ;
-//        System.out.print(k);
-        return (int) ((Math.random() * (max - min)) + min);
+
+        }
+
+
+
+        return solutionCounter;
     }
 
-    public static int climbStairs(int n) {
-//        Set<Integer> treeSet = new TreeSet<>();
+
+    public static int climbStairsTreeSet(int n) {
         Set<String> treeSet = new TreeSet<>();
+        int sumary = 0;
+        int randomPath = 0;
+        String dummyPathString = "";
+        int allPasscount = 0;
+        int allFilteredCount = 0;
+        for (Integer k = 0; k < 100000000; k++) {
+            while (sumary < n) {
+                randomPath = (int) ((Math.random() * (2)) + 1);
+
+                dummyPathString += randomPath;
+                sumary += randomPath;
+            }
+            if (sumary == n) {
+                treeSet.add(dummyPathString);
+//                allPasscount++;
+            }
+
+            sumary = 0;
+            dummyPathString = "";
+        }
+//        for (String iter : treeSet) {
+////            System.out.println("allFilteredcount=" + allFilteredCount + " iter=" + iter);
+//            allFilteredCount++;
+//        }
+        allFilteredCount = treeSet.size();
+        return allFilteredCount;
+    }
+
+    public static int climbStairshash(int n) {
+        Set<String> treeSet = new HashSet<>();
         int sumary = 0;
         int i = 0;
         int randomPath = 0;
@@ -27,30 +75,42 @@ public class Zad5 {
         int allFilteredCount = 0;
         for (Integer k = 0; k < 100000; k++) {
             while (sumary < n) {
-                randomPath =  (int) ((Math.random() * (2)) + 1);
+                randomPath = (int) ((Math.random() * (2)) + 1);
 
                 dummyPathString += randomPath;
                 sumary += randomPath;
             }
             if (sumary == n) {
                 treeSet.add(dummyPathString);
-                allPasscount++;
+//                allPasscount++;
             }
 
             sumary = 0;
             dummyPathString = "";
         }
-        for (String iter : treeSet) {
-//            System.out.println("allFilteredcount=" + allFilteredCount + " iter=" + iter);
-            allFilteredCount++;
+//        for (String iter : treeSet) {
+////            System.out.println("allFilteredcount=" + allFilteredCount + " iter=" + iter);
+//            allFilteredCount++;
+//        }
+        for(String h : treeSet){
+            System.out.println(h);
         }
+        allFilteredCount = treeSet.size();
         return allFilteredCount;
+    }
+
+    public static int getRandomNumber(int min, int max) {
+
+        ;
+//        System.out.print(k);
+        return (int) ((Math.random() * (max - min)) + min);
     }
 
     public static void main(String[] args) {
 
         Zad5 klasa = new Zad5();
-        int result = klasa.climbStairs(35);
+        System.out.println(klasa.climbStairshash(5)) ;
+//        System.out.println(klasa.climbStairs(3));
 
 
     }
