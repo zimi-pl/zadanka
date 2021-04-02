67d34f2c38c8bd4b876d8a73bd1ef52dde4d23de
sm000k <smok.lacki@gmail.com>
2021-04-02 04:51:13 +0200

    Zad 5 testy i rozwiązania. Napisałem 3 metody. Wszystkie metody dają poprawny wynik. Jednak tylko climbStairsQuicker przechodzi testy bez przekroczenia czasu na wykonanie.

--- src/main/java/pl/zimi/zadanka/Zad5.java
+++ src/main/java/pl/zimi/zadanka/Zad5.java
@@ -4,116 +4,45 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-/*
-source:
-https://leetcode.com/problems/climbing-stairs/
+/**
+ * source:
+ * https://leetcode.com/problems/climbing-stairs/
  */
+
 public class Zad5 {
-//    public void Node ( int destination,int currentPath){
-//        int left = currentPath+1;
-//        int right = destination+2;
-//    }
-    /*
-    key = destination pointer=current node
-    left/right child node
-     */
+
+    // rozwiązanie poprawne ale wolne
+    public static int climbStairsTreeStyle(int key, int currentValue, int carry) {
+        if (currentValue == key) return ++carry;
+        if (currentValue > key) return 0;
+        else
+            return climbStairsTreeStyle(key, currentValue + 1, carry)
+                    + climbStairsTreeStyle(key, currentValue + 2, carry);
+    }
+
+    // rozwiązanie poprawne ale za wolne
     public static int climbStairs(int key) {
-        int solutionCounter = 0;
-        int startPointer=1;
-        int pointer =startPointer;
-        int left=1;
-        int right=2;
+        if (key > 0) {
+            if (key == 1) return 1;
+            if (key == 2) return 2;
+            return climbStairs(key - 2) + climbStairs(key - 1);
+        } else return 0;
+    }
 
-        while ((pointer!=0)&&(pointer!=key)){
-
-
+    // rozwiazanie przyjete przez leetcode
+    public static int climbStairsQuicker(int key) {
+        if (key == 2) return 2;
+        if (key == 1) return 1;
+        int thirdValue = 0;
+        int secondValue = 2;
+        int firstValue = 1;
+        int iterVal = 3;
+        while (iterVal++ <= key) {
+            thirdValue = firstValue + secondValue;
+            firstValue = secondValue;
+            secondValue = thirdValue;
         }
-
-
-
-        return solutionCounter;
-    }
-
-
-    public static int climbStairsTreeSet(int n) {
-        Set<String> treeSet = new TreeSet<>();
-        int sumary = 0;
-        int randomPath = 0;
-        String dummyPathString = "";
-        int allPasscount = 0;
-        int allFilteredCount = 0;
-        for (Integer k = 0; k < 100000000; k++) {
-            while (sumary < n) {
-                randomPath = (int) ((Math.random() * (2)) + 1);
-
-                dummyPathString += randomPath;
-                sumary += randomPath;
-            }
-            if (sumary == n) {
-                treeSet.add(dummyPathString);
-//                allPasscount++;
-            }
-
-            sumary = 0;
-            dummyPathString = "";
-        }
-//        for (String iter : treeSet) {
-////            System.out.println("allFilteredcount=" + allFilteredCount + " iter=" + iter);
-//            allFilteredCount++;
-//        }
-        allFilteredCount = treeSet.size();
-        return allFilteredCount;
-    }
-
-    public static int climbStairshash(int n) {
-        Set<String> treeSet = new HashSet<>();
-        int sumary = 0;
-        int i = 0;
-        int randomPath = 0;
-        String dummyPathString = "";
-        int allPasscount = 0;
-        int allFilteredCount = 0;
-        for (Integer k = 0; k < 100000; k++) {
-            while (sumary < n) {
-                randomPath = (int) ((Math.random() * (2)) + 1);
-
-                dummyPathString += randomPath;
-                sumary += randomPath;
-            }
-            if (sumary == n) {
-                treeSet.add(dummyPathString);
-//                allPasscount++;
-            }
-
-            sumary = 0;
-            dummyPathString = "";
-        }
-//        for (String iter : treeSet) {
-////            System.out.println("allFilteredcount=" + allFilteredCount + " iter=" + iter);
-//            allFilteredCount++;
-//        }
-        for(String h : treeSet){
-            System.out.println(h);
-        }
-        allFilteredCount = treeSet.size();
-        return allFilteredCount;
-    }
-
-    public static int getRandomNumber(int min, int max) {
-
-        ;
-//        System.out.print(k);
-        return (int) ((Math.random() * (max - min)) + min);
-    }
-
-    public static void main(String[] args) {
-
-        Zad5 klasa = new Zad5();
-        System.out.println(klasa.climbStairshash(5)) ;
-//        System.out.println(klasa.climbStairs(3));
-
+        return thirdValue;
 
     }
-
-
 }
--- src/test/java/pl/zimi/zadanka/Zad5Test.java
+++ src/test/java/pl/zimi/zadanka/Zad5Test.java
@@ -5,9 +5,68 @@ import org.junit.jupiter.api.Test;
 
 public class Zad5Test {
     @Test
-    void n3 ()
-    {
+    void n0() {
         Zad5 object = new Zad5();
-        Assertions.assertEquals(3,object.climbStairs(3));
+        Assertions.assertEquals(0, object.climbStairs(0));
     }
+
+    @Test
+    void n3() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(3, object.climbStairs(3));
+    }
+
+    @Test
+    void n3TreeVersion() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(3, object.climbStairsTreeStyle(3, 0, 0));
+    }
+
+    @Test
+    void n3Quicker() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(3, object.climbStairsQuicker(3));
+    }
+
+    @Test
+    void n4() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(5, object.climbStairsQuicker(4));
+    }
+
+    @Test
+    void n5() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(8, object.climbStairsQuicker(5));
+    }
+
+    @Test
+    void n6() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(13, object.climbStairs(6));
+    }
+
+    @Test
+    void n6q() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(13, object.climbStairsQuicker(6));
+    }
+    @Test
+    void n44() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(1134903170, object.climbStairs(44));
+    }
+
+    @Test
+    void n45() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(1836311903, object.climbStairs(45));
+    }
+
+    @Test
+    void n45Quicker() {
+        Zad5 object = new Zad5();
+        Assertions.assertEquals(1836311903, object.climbStairsQuicker(45));
+    }
+
 }
