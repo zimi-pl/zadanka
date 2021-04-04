92ca2b593848a6e2f63ce1f9940dc9d69f3ecae6
sm000k <smok.lacki@gmail.com>
2021-04-05 01:14:33 +0200

    Zad 8 przygotowania pod obsługę nodów

--- src/test/java/pl/zimi/zadanka/Zad8Test.java
+++ src/test/java/pl/zimi/zadanka/Zad8Test.java
@@ -2,7 +2,16 @@ package pl.zimi.zadanka;
 
 import org.junit.jupiter.api.Test;
 
+
 public class Zad8Test {
+
+//    public static int revokeMaxDepth(Integer[] inputArray) {
+////        Zad7 test = new Zad7();
+//
+//        TreeNode root = test.createTree(new TreeNode(), inputArray, 0);
+//
+//        return test.maxDepth(root);
+//    }
     @Test
     void dummyMethod(){
 
