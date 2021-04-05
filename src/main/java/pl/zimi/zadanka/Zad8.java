5c08354509c0eb690a0288bebc0510a6790beaa3
sm000k <smok.lacki@gmail.com>
2021-04-06 01:18:56 +0200

    Zad 8 przygotowania pod obsługę nodów

--- src/main/java/pl/zimi/zadanka/Zad8.java
+++ src/main/java/pl/zimi/zadanka/Zad8.java
@@ -5,4 +5,31 @@ package pl.zimi.zadanka;
  */
 public class Zad8 {
 
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+//        ListNode () {}
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+   /* public static TreeNode createTree(TreeNode tempRoot, Integer[] treeElements, int pointer) {
+
+        if (pointer >= treeElements.length || treeElements[pointer] == null) return null;
+        else if (tempRoot == null) return createTree(new TreeNode(treeElements[pointer]), treeElements, pointer);
+
+        return new TreeNode(tempRoot.val,
+                (createTree(tempRoot.left, treeElements, 2 * pointer + 1)),
+                createTree(tempRoot.right, treeElements, 2 * pointer + 2));
+    }*/
+
+
+    public void deleteNode(ListNode Treenode) {
+
+    }
+
+
 }
--- src/test/java/pl/zimi/zadanka/Zad8Test.java
+++ src/test/java/pl/zimi/zadanka/Zad8Test.java
@@ -1,19 +1,37 @@
 package pl.zimi.zadanka;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class Zad8Test {
+    /**
+    public static int revokeMaxDepth(Integer[] inputArray) {
+        Zad8  test = new Zad8();
+        TreeNode root = test.createTree(new TreeNode(), inputArray, 0);
+        return test.maxDepth(root);
+    }
+     */
+    public void revokeCreateListNode (int[] inputNodesList){
+        Zad8 localClass = new Zad8();
+        Zad8.ListNode listNode = localClass.new ListNode(0);
+        listNode = createListNode(inputNodesList,listNode,0);
+    }
+    public Zad8.ListNode createListNode(int[] inputNodesList, Zad8.ListNode listNode, int i) {
 
-//    public static int revokeMaxDepth(Integer[] inputArray) {
-////        Zad7 test = new Zad7();
-//
-//        TreeNode root = test.createTree(new TreeNode(), inputArray, 0);
-//
-//        return test.maxDepth(root);
-//    }
+        if (i != inputNodesList.length) {
+            listNode.val = inputNodesList[i];
+            listNode.next = createListNode(inputNodesList,listNode,++i);
+
+        }
+
+        return listNode;
+    }
     @Test
     void dummyMethod(){
+        revokeCreateListNode(new int[] {4,5,1,9});
+//        new int[] {4,5,9}
+        Assertions.assertEquals(0,0);
 
     }
 }
