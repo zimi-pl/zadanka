9dbdc55585338f868d48670c3eb9f9427a510539
sm000k <smok.lacki@gmail.com>
2021-04-19 00:40:12 +0200

    Tworzenie plików do zadania 9

--- src/test/java/pl/zimi/zadanka/Zad9Test.java
+++ src/test/java/pl/zimi/zadanka/Zad9Test.java
@@ -5,11 +5,43 @@ import org.junit.jupiter.api.Test;
 
 public class Zad9Test {
 
+    public ListNode revokeCreateListNode(int[] inputNodesList, int headpointer) {
+        ListNode listNode = new ListNode(0);
+        listNode = createListNode(inputNodesList, listNode, 0);
+        ListNode tempNode = listNode;
+        while (tempNode.val != headpointer && tempNode.next != null) {
+            tempNode = tempNode.next;
+        }
+        return tempNode;
+
+    }
+
+    public ListNode createListNode(int[] inputNodesList, ListNode listNode, int i) {
+        if (i != inputNodesList.length - 1) {
+            listNode.val = inputNodesList[i];
+            i++;
+            listNode.next = createListNode(inputNodesList, new ListNode(inputNodesList[i]), i);
+        }
+        return listNode;
+    }
+
+    public String printListNode(ListNode listNode) {
+        String listToOutput = "";
+        listToOutput += listNode.val;
+        ListNode tempListNode = listNode;
+        while (tempListNode.next != null) {
+            tempListNode = tempListNode.next;
+            listToOutput += "," + tempListNode.val;
+        }
+        System.out.println(listToOutput);
+        return listToOutput;
+    }
+
 
     @Test
     void dummyMethodswapairs() {
         Zad9 zad8Obj = new Zad9();
-        ListNode node = revokeCreateListNode(new int[]{1, 2, 3, 4,5,6}, 5);
+        ListNode node = revokeCreateListNode(new int[]{1, 2, 3, 4, 5, 6}, 5);
 
         printListNode(node);
         Assertions.assertEquals(0, 0);
