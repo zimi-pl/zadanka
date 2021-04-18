06d019761e85e74ae69e9ecc303769afa1891aa6
sm000k <smok.lacki@gmail.com>
2021-04-19 00:39:05 +0200

    Tworzenie plików do zadania 9

---
+++ src/test/java/pl/zimi/zadanka/Zad9Test.java
@@ -0,0 +1,17 @@
+package pl.zimi.zadanka;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Zad9Test {
+
+
+    @Test
+    void dummyMethodswapairs() {
+        Zad9 zad8Obj = new Zad9();
+        ListNode node = revokeCreateListNode(new int[]{1, 2, 3, 4,5,6}, 5);
+
+        printListNode(node);
+        Assertions.assertEquals(0, 0);
+    }
+}
