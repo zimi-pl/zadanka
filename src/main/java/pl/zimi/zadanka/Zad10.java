b0b9871a7ee720cd5923903d5914d641dce0b71e
sm000k <smok.lacki@gmail.com>
2021-04-20 19:00:49 +0200

    Zad10 rozwiazanie dla pierwszego przykładu z leetcode

--- src/main/java/pl/zimi/zadanka/Zad10.java
+++ src/main/java/pl/zimi/zadanka/Zad10.java
@@ -1,4 +1,61 @@
 package pl.zimi.zadanka;
 
+
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zad10 {
+    public class SwappingState {
+        int k;
+        int iterator;
+        int iteratorMax;
+        ListNode leftTempNode;
+        ListNode rightTempNode;
+
+        public SwappingState(int k) {
+            this.k = k;
+            this.iterator = 0;
+            this.leftTempNode = null;
+            this.rightTempNode = null;
+        }
+    }
+
+    public ListNode swapNodes(ListNode head, int k) {
+        int[] temps = {k, 0, 0};
+        SwappingState swappingState = new SwappingState(k);
+        SwappingState resultSwappingState = swapNodesHelper(head, temps, swappingState);
+        return head;
+    }
+
+    public SwappingState swapNodesHelper(ListNode actualNode, int[] temps, SwappingState swappingState) {
+        if (actualNode == null) {
+            temps[2] = temps[1];
+            swappingState.iteratorMax = swappingState.iterator;
+            return swappingState;
+        }
+        swappingState.iterator++;
+        // before reaching max
+        if (swappingState.iterator == swappingState.k)
+            swappingState.leftTempNode = actualNode;
+
+        swappingState = swapNodesHelper(actualNode.next, temps, swappingState);
+
+        // after reaching max
+        if (swappingState.iteratorMax - swappingState.iterator + 1 == swappingState.k) {
+            swappingState.rightTempNode = actualNode;
+        }
+        if (swappingState.iteratorMax - swappingState.iterator == swappingState.k) {
+            actualNode.next = swappingState.leftTempNode;
+        }
+        if (swappingState.iterator == swappingState.k - 1) {
+            actualNode.next = swappingState.rightTempNode;
+            ListNode tempNode = swappingState.rightTempNode.next;
+            swappingState.rightTempNode.next = swappingState.leftTempNode.next;
+            swappingState.leftTempNode.next = tempNode;
+        }
+        swappingState.iterator--;
+        return swappingState;
+    }
 }
--- src/test/java/pl/zimi/zadanka/Zad10Test.java
+++ src/test/java/pl/zimi/zadanka/Zad10Test.java
@@ -3,10 +3,62 @@ package pl.zimi.zadanka;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Zad10Test {
-    @Test
-    public void firstExample(){
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
-        Assertions.assertArrayEquals(new int[]{0}, new int[]{0});
+public class Zad10Test {
+    public ListNode revokeCreateListNode(Integer [] inputNodesList) {
+        ListNode listNode = new ListNode(0);
+        listNode = createListNode(inputNodesList, listNode, 0);
+        return listNode;
+
+    }
+
+    public ListNode createListNode(Integer [] inputNodesList, ListNode listNode, int i) {
+        if (inputNodesList.length == 0) {
+            return null;
+        }
+        if (inputNodesList.length == 1) {
+            listNode.val = inputNodesList[i];
+            return listNode;
+        }
+        if (i != inputNodesList.length - 1) {
+            listNode.val = inputNodesList[i];
+            i++;
+            listNode.next = createListNode(inputNodesList, new ListNode(inputNodesList[i]), i);
+        }
+        return listNode;
+    }
+
+    public String printListNode(ListNode listNode) {
+        String listToOutput = "";
+        listToOutput += listNode.val;
+        ListNode tempListNode = listNode;
+        while (tempListNode.next != null) {
+            tempListNode = tempListNode.next;
+            listToOutput += "," + tempListNode.val;
+        }
+        System.out.println(listToOutput);
+        return listToOutput;
+    }
+
+    public ArrayList<Integer> ListNodeToArray(ListNode listNode) {
+        ArrayList<Integer> array = new ArrayList<>();
+        ListNode tempListNode = listNode;
+        while (tempListNode != null) {
+            array.add(tempListNode.val);
+            tempListNode = tempListNode.next;
+        }
+        return array;
+    }
+
+    @Test
+    public void firstExample() {
+        Zad10 obj = new Zad10();
+        Integer inputArray[]= {1,2,3,4,5,6,7};
+        ListNode listNodeInput = revokeCreateListNode(inputArray);
+        obj.swapNodes(listNodeInput,2);
+        Assertions.assertArrayEquals(new Integer[]{0}, new Integer []{0});
     }
 }
