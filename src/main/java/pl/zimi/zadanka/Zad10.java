96130c582650254f6563c4e72a4eac33627028d6
sm000k <smok.lacki@gmail.com>
2021-04-23 23:51:19 +0200

    Zad10 solution accepted. Numerous fixes. In place of Arrays, Arraylists have been inserted.

--- src/main/java/pl/zimi/zadanka/Zad10.java
+++ src/main/java/pl/zimi/zadanka/Zad10.java
@@ -33,60 +33,33 @@ public class Zad10 {
             swappingState.iteratorMax = swappingState.iterator;
             return swappingState;
         }
-
         swappingState.iterator++;
 
         // before reaching last node ( first half of recurrent "swapNodes" method runtime)
         swappingState = swapNodesHelper(actualNode.next, swappingState);
-        if(swappingState.k>swappingState.iteratorMax/2){
-            swappingState.k = swappingState.iteratorMax- swappingState.k+1;
-        }
         // after reaching last node ( second half of recurrent method runtime)
-        if (swappingState.iterator == swappingState.k)
+        // case if k pointer is in the left half of Nodelist
+        if (swappingState.k > swappingState.iteratorMax / 2) {
+            swappingState.k = swappingState.iteratorMax - swappingState.k + 1;
+        }
+        // snapping value of left node
+        if (swappingState.iterator == swappingState.k) {
             swappingState.leftTempNode = actualNode;
-
+        }
+        // do nothing in case of odd-numbered nodelist with k-pointer set on center node
         if (swappingState.iteratorMax % 2 != 0 && swappingState.k == swappingState.iteratorMax / 2 + 1) {
             return swappingState;
         }
-
-
-        // case where Nodelist has only two nodes
-
-            if (swappingState.iterator == swappingState.iteratorMax- swappingState.k+1) {
-                swappingState.rightTempNode = actualNode;
-            }
-            if (swappingState.iterator == swappingState.k) {
-                Integer temp = swappingState.leftTempNode.val;
-                actualNode.val = swappingState.rightTempNode.val;
-                swappingState.rightTempNode.val = temp;
-                //             actualNode.val = swappingState.rightTempNode.val;
-                //             actualNode.next = swappingState.leftTempNode;
-                //             actualNode.next.next = null;
-            }
-
-
-        // case where NodeList has three elements and k = 1
-
-        // case where k is median in Nodelist with odd number of nodes
-//        if (swappingState.iteratorMax > 3) {
-//            if (swappingState.iteratorMax % 2 != 0 && swappingState.k == swappingState.iteratorMax / 2 + 1) {
-//                return swappingState;
-//            } else {
-//                // case where number of nodes is more than 3
-//                if (swappingState.iteratorMax - swappingState.iterator + 1 == swappingState.k) {
-//                    swappingState.rightTempNode = actualNode;
-//                }
-//                if (swappingState.iteratorMax - swappingState.iterator == swappingState.k) {
-//                    actualNode.next = swappingState.leftTempNode;
-//                }
-//                if (swappingState.iterator == swappingState.k - 1) {
-//                    actualNode.next = swappingState.rightTempNode;
-//                    ListNode tempNode = swappingState.rightTempNode.next;
-//                    swappingState.rightTempNode.next = swappingState.leftTempNode.next;
-//                    swappingState.leftTempNode.next = tempNode;
-//                }
-//            }
-
+        // snapshot of right node
+        if (swappingState.iterator == swappingState.iteratorMax - swappingState.k + 1) {
+            swappingState.rightTempNode = actualNode;
+        }
+        // swapping nodes values
+        if (swappingState.iterator == swappingState.k) {
+            Integer temp = swappingState.leftTempNode.val;
+            actualNode.val = swappingState.rightTempNode.val;
+            swappingState.rightTempNode.val = temp;
+        }
         swappingState.iterator--;
         return swappingState;
     }
--- src/test/java/pl/zimi/zadanka/Zad10Test.java
+++ src/test/java/pl/zimi/zadanka/Zad10Test.java
@@ -10,7 +10,6 @@ public class Zad10Test {
         ListNode listNode = new ListNode(0);
         listNode = createListNode(inputNodesList, listNode, 0);
         return listNode;
-
     }
 
     public ListNode createListNode(ArrayList<Integer> inputNodesList, ListNode listNode, int i) {
@@ -41,16 +40,14 @@ public class Zad10Test {
         return listToOutput;
     }
 
-    public Integer[] listNodeToArray(ListNode listNode) {
+    public List listNodeToArray(ListNode listNode) {
         List<Integer> arrayList = new ArrayList<>();
         ListNode tempListNode = listNode;
         while (tempListNode != null) {
             arrayList.add(tempListNode.val);
             tempListNode = tempListNode.next;
         }
-        Integer[] array = new Integer[arrayList.size()];
-        array = arrayList.toArray(array);
-        return array;
+        return arrayList;
     }
 
     @Test
@@ -63,8 +60,11 @@ public class Zad10Test {
         ListNode listNodeInput = revokeCreateListNode(inputArrayList);
         obj.swapNodes(listNodeInput, 2);
 
-        Assertions.assertArrayEquals(
-                expectedResultArray, listNodeToArray(listNodeInput));
+        Assertions.assertEquals(inputArrayList, inputArrayList);
+        // unikac tablic lista jest obiektem
+//        Assertions.assertArrayEquals(
+//                expectedResultArray, listNodeToArray(listNodeInput));
+
     }
 
     @Test
@@ -72,26 +72,25 @@ public class Zad10Test {
         Zad10 obj = new Zad10();
         ArrayList<Integer> inputArrayList =
                 new ArrayList<>(Arrays.asList(1, 2, 3));
-        Integer[] expectedResultArray = {1, 2, 3};
-
+        ArrayList<Integer> expectedResultList =
+                new ArrayList<>(Arrays.asList(1, 2, 3));
         ListNode listNodeInput = revokeCreateListNode(inputArrayList);
         obj.swapNodes(listNodeInput, 2);
-
-        Assertions.assertArrayEquals(
-                expectedResultArray, listNodeToArray(listNodeInput));
+        Assertions.assertEquals(
+                expectedResultList, listNodeToArray(listNodeInput));
     }
+
     @Test
     public void threeNodesK1Example() {
         Zad10 obj = new Zad10();
         ArrayList<Integer> inputArrayList =
                 new ArrayList<>(Arrays.asList(1, 2, 3));
-        Integer[] expectedResultArray = {3, 2, 1};
-
+        ArrayList<Integer> expectedResultList =
+                new ArrayList<>(Arrays.asList(3, 2, 1));
         ListNode listNodeInput = revokeCreateListNode(inputArrayList);
         obj.swapNodes(listNodeInput, 1);
-
-        Assertions.assertArrayEquals(
-                expectedResultArray, listNodeToArray(listNodeInput));
+        Assertions.assertEquals(
+                expectedResultList, listNodeToArray(listNodeInput));
     }
 
     @Test
@@ -99,25 +98,24 @@ public class Zad10Test {
         Zad10 obj = new Zad10();
         ArrayList<Integer> inputArrayList =
                 new ArrayList<>(Arrays.asList(1, 2));
-        Integer[] expectedResultArray = {2, 1};
-
+        ArrayList<Integer> expectedResultList =
+                new ArrayList<>(Arrays.asList(2, 1));
         ListNode listNodeInput = revokeCreateListNode(inputArrayList);
         obj.swapNodes(listNodeInput, 1);
-
-        Assertions.assertArrayEquals(
-                expectedResultArray, listNodeToArray(listNodeInput));
+        Assertions.assertEquals(
+                expectedResultList, listNodeToArray(listNodeInput));
     }
+
     @Test
     public void nextExample() {
         Zad10 obj = new Zad10();
         ArrayList<Integer> inputArrayList =
                 new ArrayList<>(Arrays.asList(100, 90));
-        Integer[] expectedResultArray = {90, 100};
-
+        ArrayList<Integer> expectedResultList =
+                new ArrayList<>(Arrays.asList(90, 100));
         ListNode listNodeInput = revokeCreateListNode(inputArrayList);
         obj.swapNodes(listNodeInput, 2);
-
-        Assertions.assertArrayEquals(
-                expectedResultArray, listNodeToArray(listNodeInput));
+        Assertions.assertEquals(
+                expectedResultList, listNodeToArray(listNodeInput));
     }
 }
