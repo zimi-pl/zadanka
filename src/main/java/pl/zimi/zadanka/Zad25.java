313f09d0bd3ae093dfa0aede9d5b57b29835b5fd
sm000k <smok.lacki@gmail.com>
2021-07-05 03:12:05 +0200

    Zad25 quick solution and tests

--- src/main/java/pl/zimi/zadanka/Zad25.java
+++ src/main/java/pl/zimi/zadanka/Zad25.java
@@ -1,8 +1,32 @@
 package pl.zimi.zadanka;
 
+//https://leetcode.com/problems/merge-two-sorted-lists/
 public class Zad25 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode nodeTemp1 = l1;
+        ListNode nodeTemp2 = l2;
 
-        return new ListNode();
+        ListNode L3 = new ListNode(0);
+        ListNode marker = L3;
+
+        int tempVal = 0;
+        while (nodeTemp1 != null || nodeTemp2 != null) {
+            if (nodeTemp1 != null && (nodeTemp2 == null || nodeTemp1.val < nodeTemp2.val)) {
+                tempVal = nodeTemp1.val;
+                nodeTemp1 = nodeTemp1.next;
+            } else {
+                if (nodeTemp2 != null && (nodeTemp1 == null || nodeTemp1.val >= nodeTemp2.val)) {
+                    tempVal = nodeTemp2.val;
+                    nodeTemp2 = nodeTemp2.next;
+                }
+            }
+            marker.next = new ListNode(tempVal);
+            marker = marker.next;
+        }
+        L3 = L3.next;
+        return L3;
     }
 }
--- src/test/java/pl/zimi/zadanka/Zad25Test.java
+++ src/test/java/pl/zimi/zadanka/Zad25Test.java
@@ -3,17 +3,31 @@ package pl.zimi.zadanka;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//https://leetcode.com/problems/merge-two-sorted-lists/
 public class Zad25Test {
+    List printAndMakeArrayOfNodes(ListNode result) {
+        List<Integer> array = new ArrayList<>();
+        while (result != null) {
+            System.out.println(result.val);
+            array.add(result.val);
+            result = result.next;
+        }
+        return array;
+    }
+
     ListNode createListNode(int[] inputArray) {
         ListNode node;
-        if (inputArray == null) {
+        if (inputArray.length == 0) {
             return null;
         } else {
             node = new ListNode(inputArray[0]);
-            ListNode currentNode=node;
-            for (int i =1; i<inputArray.length;i++) {
-                currentNode.next=new ListNode(inputArray[i]);
-                currentNode=currentNode.next;
+            ListNode currentNode = node;
+            for (int i = 1; i < inputArray.length; i++) {
+                currentNode.next = new ListNode(inputArray[i]);
+                currentNode = currentNode.next;
             }
 
         }
@@ -23,7 +37,34 @@ public class Zad25Test {
     @Test
     void dummyTest() {
         Zad25 solution = new Zad25();
-        ListNode x = createListNode(new int[]{1, 2, 3, 4});
-        Assertions.assertEquals(1, 1);
+        ListNode x = createListNode(new int[]{0, 2, 4, 6, 8, 9, 10, 10, 10});
+        ListNode y = createListNode(new int[]{1, 3, 5, 7, 9, 10});
+        ListNode result = solution.mergeTwoLists(x, y);
+        Assertions.assertEquals(
+                Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 10, 10, 10),
+                printAndMakeArrayOfNodes(result));
+    }
+
+    @Test
+    void firstTest() {
+        Zad25 solution = new Zad25();
+        ListNode result = solution.mergeTwoLists(createListNode(new int[]{1, 2, 4}), createListNode(new int[]{1, 3, 4}));
+        Assertions.assertEquals(Arrays.asList(1, 1, 2, 3, 4, 4), printAndMakeArrayOfNodes(result));
+    }
+
+    @Test
+    void secondTest() {
+        Zad25 solution = new Zad25();
+        ListNode result = solution.mergeTwoLists(createListNode(new int[]{}), createListNode(new int[]{0}));
+        Assertions.assertEquals(Arrays.asList(0), printAndMakeArrayOfNodes(result));
+    }
+
+    @Test
+    void thirdTest() {
+        Zad25 solution = new Zad25();
+        ListNode result = solution.mergeTwoLists(
+                createListNode(new int[]{}),
+                createListNode(new int[]{}));
+        Assertions.assertEquals(Arrays.asList(), printAndMakeArrayOfNodes(result));
     }
 }
